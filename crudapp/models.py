36cb5dfe23188840217e4e3b105a03f64d3d1733
Alexandr.Besan <alexandr.besan@gmail.com>
2024-02-17 15:45:52 +0200

    Models: add str to objects

--- crudapp/models.py
+++ crudapp/models.py
@@ -5,11 +5,18 @@ class Client(models.Model):
     last_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
     class Meta:
         app_label = 'crudapp'
+
 class Operator(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
     class Meta:
         app_label = 'crudapp'
@@ -25,5 +32,8 @@ class Request(models.Model):
     client = models.ForeignKey(Client, related_name='requests', on_delete=models.CASCADE)
     processed_by = models.ForeignKey(Operator, related_name='processed_requests', on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return f"Request {self.id} - Status: {self.status}"
+    
     class Meta:
         app_label = 'crudapp'
